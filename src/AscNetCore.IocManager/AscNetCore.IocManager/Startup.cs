fcc8f577d8c7044abb56e472102e81d65c1cb867
刘伟超 <liuweic.@52mf.com>
2019-10-22 16:42:12 +0800

    fix

--- src/AscNetCore.IocManager/AscNetCore.IocManager/Startup.cs
+++ src/AscNetCore.IocManager/AscNetCore.IocManager/Startup.cs
@@ -24,8 +24,6 @@ namespace AscNetCore.IocManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            ServiceCollection serviceDescriptors = new ServiceCollection();
-            serviceDescriptors.b
             services.UseService();
 
             services.AddControllersWithViews();
